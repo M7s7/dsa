ee1743c34e11e53447e3b91c0f39a1cf14a6b621
Michael Siu <michaelsiu01@gmail.com>
2022-04-22 01:47:22 +1000

    Add new files

---
+++ sliding-window/03_Longest_substring_with_k_distinct_characters.py
@@ -0,0 +1,12 @@
+def main():
+    string = "araaci"
+    k = 2
+    print(longest_substring_with_k_distinct(string, k))
+
+def longest_substring_with_k_distinct(str, k):
+    # Iterate over the entire array from index 0
+    for windowEnd in range(len(str)):
+        
+
+
+main()
