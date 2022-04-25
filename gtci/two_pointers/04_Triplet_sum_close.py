caa910908656ca9080abd965893257f7da8cccc6
Michael Siu <michaelsiu01@gmail.com>
2022-04-25 23:01:13 +1000

    Add 3Sum Close

--- gtci/two_pointers/04_Triplet_sum_close.py
+++ gtci/two_pointers/04_Triplet_sum_close.py
@@ -1,11 +1,12 @@
 # Given an array of unsorted numbers and a target number, find a triplet in the array whose sum is as close to the target number as possible, return the sum of the triplet. 
 # If there are more than one such triplet, return the sum of the triplet with the smallest sum.
+# Time complexity is O(N^2) (+ nlogn for the sort) // Space complexity is O(N) for sorting. 
 
 import math
 
 def main():
-    arr = [ 0, -1, -1, 1]
-    target = 0
+    arr = [-3, -1, 1, 2]
+    target = 1
     print(threeSum_close(arr, target))
 
 
@@ -25,14 +26,17 @@ def threeSum_close(nums, target):
         while left < right: 
             # Calculate sum of triplet. Check if it is the current closest; if so, store value
             threeSum = nums[index] + nums[left] + nums[right]
-            if abs(threeSum - target) <= diff:
+            if abs(threeSum - target) < diff:
                 # Checking if the current sum is smaller than stored sum. If so, record that result instead
-                if threeSum < result:
-                    result = min(threeSum, result)
+                result = threeSum
+                # New diff as benchmark
                 diff = abs(threeSum - target)
+            # Returns the smallest number if difference is the same
+            elif abs(threeSum - target) == diff:
+                result = min(threeSum, result)
             # If the difference is zero, we have a solution 
             if threeSum == target:
-                return [nums[index], nums[left], nums[right]]
+                return target
             
             # If the difference between threeSum and target is positive, we need to decrement right to lower value
             elif threeSum - target > 0: 
@@ -40,5 +44,6 @@ def threeSum_close(nums, target):
             else:
                 left += 1
     return result
+
 
 main()
