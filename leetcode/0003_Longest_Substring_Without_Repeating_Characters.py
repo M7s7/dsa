e90f97765d6f015dd0183ab0213e8deed00451ef
Michael Siu <michaelsiu01@gmail.com>
2022-05-01 02:41:40 +1000

    Improve answer

--- leetcode/0003_Longest_Substring_Without_Repeating_Characters.py
+++ leetcode/0003_Longest_Substring_Without_Repeating_Characters.py
@@ -1,23 +1,50 @@
-def main():
-    str = "abba"
-    print(lengthOfLongestSubstring(str))
+# Given a string s, find the length of the longest substring without repeating characters.
+# The efficient methods use SLIDING WINDOW techniques.
 
-
-def lengthOfLongestSubstring(s):
-    # Initiate variables
-    i, max_length = 0, 0
-    # Create hashmap to store location of variables
-    index = {}
-    # Increment over the loop
-    for j in range(len(s)): 
-        # If added character is not unique, move i to just beyond the old index 
-        if s[j] in index:
-            i = max(i, index[s[j]] + 1)
-        # Regardless, store the character in a new location/initial location
-        index[s[j]] = j
-        # Record the maximum length
-        max_length = max(max_length, j - i + 1) 
+# METHOD 1: Sliding window: Hashmap storing locations
+# This is the optimal solution. Create a hashmap to store every letter's location, and when a duplicate is found, we can directly shrink the window, avoiding that letter.
+# However, make sure that you aren't moving the pointer backwards by taking a MAX. 
+# Time complexity: O(N) // Space complexity: O(min(m, n)) where m is 26 and n is the string length
+def lengthOfLongestSubstring(self, s: str):
+    # Create a hashmap of indices of every letter
+    locations = {}
+    left = 0
+    max_length = 0
+    
+    # Move through the string - using enumerate over range(len) is more pythonic and cleaner
+    for right, char in enumerate(s):
+        # Condition checking if there is a duplicate
+        if char in locations:
+            # We want to update the left pointer to move beyond the duplicate character index, stored in the dictionary
+            # However, for strings like "abba", we do not want the left pointer (if it has already moved because of a duplicate) to go backwards, because we will run into a previous duplicate.
+            left = max(left, locations[char] + 1)
+        # In either case, update the hashmap with a new location
+        locations[char] = right
+        # Return max length
+        windowSize = right - left + 1
+        max_length = max(max_length, windowSize)
     return max_length
 
 
-main()
+# METHOD 2: Sliding window: Hashmap storing frequency
+    # Create a hashmap of frequency
+def lengthOfLongestSubstring(self, s: str):
+    freq = {}
+    left = 0
+    max_length = 0
+    
+    # Iterate over the string
+    for right, char in enumerate(s):
+        # Shrink the left side of the window until the duplicate is removed
+        while char in freq:
+            if s[left] in freq:
+                del freq[s[left]]
+            # Keep moving the left pointer until a duplicate is found
+            left += 1
+        # In either case, store the frequency
+        freq[char] = True
+        max_length = max(max_length, right - left + 1)
+    return max_length
+
+
+## The other method is BRUTE FORCE by searching every substring. Time complexity is n^3.
