a5c846b92950a00b5140285f8bd5f45ceb29a49d
Michael Siu <michaelsiu01@gmail.com>
2022-05-01 14:30:57 +1000

    Modify sort colours

--- leetcode/0075_Sort_Colors.py
+++ leetcode/0075_Sort_Colors.py
@@ -1,5 +1,8 @@
 # Sort an array in place. The array may have a mix of 0s, 1s and 2s.
 # You must solve this problem without using the library's sort function.
+
+
+# METHOD 1: Optimal Two Pointer solution
 # Initialise three pointers - left stores 0s, right stores 2s and index scans the array. 
 # Time complexity is O(N) // Space complexity is O(1)
 
@@ -25,3 +28,36 @@ def sortColors(self, nums: List[int]) -> None:
             # Since index has been swapped with an unknown number that was on the right pointer, we have to check it again. We can do this by stalling index
             i -= 1
         i+= 1
+
+
+# OPTION 2: BUCKETSORT - store frequency of values and then rebuild the array from the start
+# Quite messy, but it is still Time Complexity of O(N) (albeit with a larger constant, seems like its 4N) // Space complexity is O(1) (as we always only store 3 values in the hashmap)
+def sortColors(self, nums: List[int]) -> None:
+    """
+    Do not return anything, modify nums in-place instead.
+    """
+    # Bucketsort solution
+    freq = {0: 0, 1: 0, 2: 0}
+    
+    for value in nums:
+            freq[value] += 1
+            
+    # Change in-place
+    index = 0
+    
+    print(freq)
+    while freq[0] > 0:
+        nums[index] = 0
+        index += 1
+        freq[0] -= 1
+        
+    while freq[1] > 0:
+        nums[index] = 1
+        index += 1
+        freq[1] -= 1   
+        
+    while freq[2] > 0: 
+        nums[index] = 2
+        index += 1
+        freq[2] -= 1
+    return
