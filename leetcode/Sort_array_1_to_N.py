3f7f3c8a7613d844f038360e42e6f27d11682545
Michael Siu <michaelsiu01@gmail.com>
2022-05-03 21:49:22 +1000

    Add sort array

--- leetcode/Sort_array_1_to_N.py
+++ leetcode/Sort_array_1_to_N.py
@@ -1,6 +1,7 @@
 # You are given an array that contains 1 to n elements. Your task is to sort this array in an efficient way.
 
-
+# We use cyclic sort. For each index, we check if the number is in the right spot. If it isn't, we swap the number with where it should be, and keep doing it. 
+# Time complexity: O(N), as we swap a maximum of N times // Space complexity: O(1) in place sorting
 def main():
     nums = [8,6,9,4,3,7,2,6,1]
     print(cyclic_sort(nums))
@@ -21,3 +22,5 @@ def cyclic_sort(nums):
     return nums
 
 main()
+
+## The other option is obviously sorting normally, but time complexity is O(N log N) and space is O(N)
