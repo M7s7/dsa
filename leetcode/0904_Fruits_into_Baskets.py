82ef4bc9c1b51dd6d074a7269dad915961e1ea1d
Michael Siu <michaelsiu01@gmail.com>
2022-05-04 21:31:41 +1000

    Add Fruits

--- leetcode/0904_Fruits_into_Baskets.py
+++ leetcode/0904_Fruits_into_Baskets.py
@@ -28,3 +28,26 @@ def totalFruit(fruits):
         # Number of fruits is equal to the size of the window
         maxFruits = max(maxFruits, j - i + 1)
     return maxFruits
+
+
+
+##  Alternate implementation, where window is not constantly shrunk but rather shrunk once. Return the size of the current window
+
+def totalFruit(self, fruits):
+    freq = {}
+    start = 0
+    for i, fruit in enumerate(fruits):
+        # Add elements to the window
+        if fruit not in freq: 
+            freq[fruit] = 1
+        else: freq[fruit] += 1
+    
+        # Check if window is valid - if not, we remove an element
+        if len(freq) > 2:
+            first = fruits[start]
+            freq[first] -= 1
+            start += 1
+            if freq[first] == 0:
+                del freq[first]
+
+    return i - start + 1
