ed690205721a4bc52a093d1bfaf99f9d18336522
Michael Siu <michaelsiu01@gmail.com>
2022-06-25 13:55:07 +1000

    TS complexity

--- leetcode/0260_Single_Number_III.py
+++ leetcode/0260_Single_Number_III.py
@@ -1,6 +1,8 @@
 # Given an integer array nums, in which exactly two elements appear only once and all the other elements appear exactly twice. Find the two elements that appear only once. You can return the answer in any order.
 
 # Bitwise operations - XOR to find A^B. Since the two numbers are distinct, A^B will be non-zero (thus, there will be at least one set bit). We can use this setbit to partition the array and XOR it again. 
+    # Time Complexity: O(N) - unsure how long the bitshifting would take 
+    # Space Compl
 class Solution:
     def singleNumber(self, nums: List[int]) -> List[int]:
         # Find A XOR B
@@ -11,7 +13,7 @@ class Solution:
         # Find a set bit - this bit will be different between the two target numbers
         set_bit = 1
         while two_xor & set_bit == 0:
-            set_bit = set_bit << 1
+            set_bit = set_bit << 1 # Same thing as saying set_bit * 2
         
         # Use the set bit to partition the array and get one target number
         target1 = 0
