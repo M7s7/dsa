529b73524bb7276e0bec093f410466d43de5c7f4
Michael Siu <michaelsiu01@gmail.com>
2022-07-16 15:59:54 +1000

    Add recursion

--- leetcode/0226_Invert_Binary_Tree.py
+++ leetcode/0226_Invert_Binary_Tree.py
@@ -1,24 +1,40 @@
 # Given the root of a binary tree, invert the tree, and return its root.
 
 # BFS: Simply go through each node of the tree, swapping the left branch with the right one. 
-    # Time Complexity: O(N) // Space Complexity: O(N)
+    # Time Complexity: O(N) 
+    # Space Complexity: O(N)
 import collections
 
-def invertTree(self, root):
-    if not root:
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return root
+        
+        queue = collections.deque()
+        queue.append(root)
+        
+        while queue:
+            length = len(queue)
+            for _ in range(length):
+                node = queue.popleft()
+                node.right, node.left = node.left, node.right
+                if node.right:
+                    queue.append(node.right)
+                if node.left:
+                    queue.append(node.left)
+        
         return root
-    
-    queue = collections.deque()
-    queue.append(root)
-    
-    while queue:
-        node = queue.popleft()
+
+
+# DFS implementation.
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        # Go to leaf node (leaf nodes can still swap their none nodes)
+        if not root:
+            return root
         
-        if node:
-            temp = node.left
-            node.left = node.right
-            node.right = temp
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        root.left, root.right = root.right, root.left
         
-            queue.append(node.left)
-            queue.append(node.right)
-    return root
+        return root
