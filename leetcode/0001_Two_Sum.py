265e9250f1ec3b09d8f6f7153c5c30398092e240
Michael Siu <michaelsiu01@gmail.com>
2022-07-20 21:23:14 +1000

    Add

--- leetcode/0001_Two_Sum.py
+++ leetcode/0001_Two_Sum.py
@@ -7,22 +7,15 @@
 # This method allows us to avoid needed to check if we are using the same element twice (eg. if the array was 0, 1, 1, 2 it would make sure that we don't use the same one twice and instead we would use both indices 1 and 2)
 # Time complexity: O(N) // Space complexity: O(N)
 
-def twoSum(self, nums, target):
-    # Create hashmap
-    indices = {}
-    
-    # Iterate through the array
-    for index, value in enumerate(nums):
-        # We want to find the other number that adds with our iterating number to get target
-        number = target - value
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Store number:index
+        values = {}
         
-        if number in indices:
-            return index, indices[number]
-        # If the number is not in the hashmap, store the current number in the hashmap and go to the next iteration
-        else: 
-            indices[value] = index    
-    return
-
-# Other two methods: BRUTE FORCE and TWO-PASS HASHMAP where we store the locations at the start and then check if we use the same element during the iteration
-# Brute force: Time complexity: O(N^2), Space complexity: O(1)
-# 2 Pass Hash: Time: O(N (actually 2N)) // Space complexity: O(N)
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in values:
+                return [i, values[complement]]
+            values[num] = i
+        
+        return False
