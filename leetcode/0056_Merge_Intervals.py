9e9c9663a977387341e89af5e07350be48cf4018
Michael Siu <michaelsiu01@gmail.com>
2022-07-23 04:29:31 +1000

    add

--- leetcode/0056_Merge_Intervals.py
+++ leetcode/0056_Merge_Intervals.py
@@ -3,9 +3,11 @@
 # We sort the intervals (which is a list of lists) by the START value of each interval. 
 # Then, starting from the first interval, we check if there is an overlap (if previous list end is larger than/equal to current list start.)
 # If there is an overlap, merge the lists by changing the END VALUE of the last stored list (if there is a new maximum). If there is not overlap, store the next list and repeat the process.  
-# Time complexity: O(nlogn (for sorting)) + N for iteration over the intervals // Space complexity: O(N) (N + N for storing every interval in the list + sorting)
+    # Time complexity: O(nlogn (for sorting)) + N for iteration over the intervals
+    #  Space complexity: O(N) (N + N for storing every interval in the list + sorting)
 
-def merge(self, intervals):
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         # First, sort the list of lists by FIRST ELEMENT of each list
         intervals.sort(key = lambda list: list[0])
         
@@ -29,10 +31,24 @@ def merge(self, intervals):
                 ans.append([start, end])
         return ans
 
-# Other solution is using CONNECTED COMPONENTS. However, this one is convoluted and not more efficient. 
-
-
-### Important new functions that I have learnt:
-# for sorting: sort(key = lambda <insert function here)
-# list[1][2]: For a list of lists, go into the FIRST INDEXED LIST. In that list, go to the SECOND INDEXED ELEMENT. 
-# list[-1]: In the list, go to the LAST ELEMENT. list[-2] is the second last element, etc. 
+# My implementation - I think it is easier to understand. 
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key = lambda x: x[0])
+        merged = []
+        
+        # Use our first interval to compare with other intervals
+        prev_start, prev_end = intervals[0]
+        for i in range(1, len(intervals)):
+            curr_start, curr_end = intervals[i]
+            # If there is no overlap, we can append the previous interval and then store our current one
+            if curr_start > prev_end: 
+                merged.append([prev_start, prev_end])
+                prev_start = curr_start
+                prev_end = curr_end
+            # If there is an overlap, extend our previous interval
+            else:
+                prev_end = max(prev_end, curr_end)
+        # Append the interval we are storing at the end of the list
+        merged.append([prev_start, prev_end])
+        return merged
