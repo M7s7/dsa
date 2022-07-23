15c6415fa46a233350121191fe285ed771e34751
Michael Siu <michaelsiu01@gmail.com>
2022-07-23 15:56:17 +1000

    Add

--- leetcode/0055_Jump_Game.py
+++ leetcode/0055_Jump_Game.py
@@ -2,7 +2,7 @@
 # You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
 # Return true if you can reach the last index, or false otherwise.
 
-# Greedy approach. Go backwards through the array, 
+# Greedy approach. Go backwards through the array, shifting the goal.
     # Intuition: We can reach every index in an array with no zeroes. Thus, we can move our goals back until we reach a zero.
     # Then, we can check if there is an index to the left of zero that can jump over the gap.
 # Time Complexity: O(N)
